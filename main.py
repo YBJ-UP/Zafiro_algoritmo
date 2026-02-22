9a198399f4ca62f36094eb0ee313ab8856c637db
Yael <243678@ids.upchiapas.edu.mx>
2026-02-22 12:06:57 -0600

    q raro es python

--- main.py
+++ main.py
@@ -23,12 +23,12 @@ yo:persona = {
 class persona_dataclass:
     nombre:str
     apellidos:str
-    fecha_nacimiento = None
     literal:Literal[1,2,3,4]
-    opcional:Optional[int]
     arreglo:list[bool]
+    fecha_nacimiento:None = None
+    opcional:Optional[int] = None #a estos se les puede poner valores predeterminados para q no de lata al no ponerlos al construirlos, a diferencia de los typeddict que no dejan hacer eso
 
-yo2 = persona_dataclass("yo", "yppp", 3, None,[])
+yo2 = persona_dataclass("yo", "yppp", 3, [], opcional=2783)
 
 def saludar(nombre:Optional[str] = None): #me hace ruido que no sea como en ts pero bueno
     return f"hola ${nombre}" if nombre else "dime tu nombre" #analogo al a? b : c de ts
