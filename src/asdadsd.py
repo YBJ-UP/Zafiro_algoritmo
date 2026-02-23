fe9baece7bad4914e6d9086b9472010427bd0f53
Yael <243678@ids.upchiapas.edu.mx>
2026-02-23 14:17:24 -0600

    ya funciona la api

---
+++ src/asdadsd.py
@@ -0,0 +1,47 @@
+from dataclasses import dataclass
+from typing import Optional,TypedDict, Literal
+# peleandome por ponerle tipado a la cosa esta
+
+class persona(TypedDict): #basicamente un tipo (o es una interfaz? ni se q tienen d diferente)
+    nombre:str
+    apellidos:str
+    fecha_nacimiento:None #noc como poner fechas aca pero bueno
+    literal:Literal[1, 2, 3, 4]
+    opcional:Optional[int]
+    arreglo:list[bool]
+
+yo:persona = {
+    "nombre":"yael",
+    "apellidos":"betanzos jimenez",
+    "fecha_nacimiento":None,
+    "arreglo":[],
+    "literal":3,
+    "opcional":None
+}
+
+@dataclass
+class persona_dataclass:
+    nombre:str
+    apellidos:str
+    literal:Literal[1,2,3,4]
+    arreglo:list[bool]
+    fecha_nacimiento:None = None
+    opcional:Optional[int] = None #a estos se les puede poner valores predeterminados para q no de lata al no ponerlos al construirlos, a diferencia de los typeddict que no dejan hacer eso
+
+yo2 = persona_dataclass("yo", "yppp", 3, [], opcional=2783)
+
+def saludar(nombre:Optional[str] = None): #me hace ruido que no sea como en ts pero bueno
+    return f"hola {nombre}" if nombre else "dime tu nombre" #analogo al a? b : c de ts
+
+
+print(saludar('yael'))
+print(yo["nombre"])
+print(yo2.nombre)
+
+def dostipos(hola:int | str):
+    if isinstance(hola, int):
+        print(f"El parámetro es un número que vale {hola}")
+    else:
+        print(f"El parámetro es la cadena: {hola}")
+    
+dostipos('nnnnnnnn')
