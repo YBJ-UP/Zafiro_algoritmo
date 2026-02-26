4940cd6c5aa8852455ebc4388fba9519367dc47a
Yael <243678@ids.upchiapas.edu.mx>
2026-02-25 18:31:25 -0600

    cambios a criterio del chatcito

--- src/model/input/calendar.py
+++ src/model/input/calendar.py
@@ -1,37 +1,69 @@
-from dataclasses import dataclass
 from datetime import date, datetime
-from typing import Literal
+from typing import Literal, NotRequired, TypedDict
 
-@dataclass
-class creator:
+class creator_dict(TypedDict):
     email:str
-    self:bool
+    self:NotRequired[bool]
 
-@dataclass
-class start:
-    date:date
+class date_dict(TypedDict):
+    date:NotRequired[str]
+    dateTime:NotRequired[str]
+    timeZone:NotRequired[str]
 
-@dataclass
-class end:
-    date:date
+class reminder_override(TypedDict):
+    method:Literal["email","popup"]
+    minutes:int
 
+class reminder_dict(TypedDict):
+    useDefault:bool
+    overrides:NotRequired[list[reminder_override]]
 
+class extras_dict(TypedDict):
+    prioridad: Literal["baja","media","alta"]
+    etiquetas: list[str]
 
-@dataclass
-class agenda:
+class agenda(TypedDict):
     kind:str
     etag:str
     id:str
     status: Literal["confirmed","tentative","cancelled"]
     htmlLink:str
-    created:datetime
-    updated:datetime
-    summary:str
-    creator: creator
-    start:start
-    end:end
-    transparency:Literal["transparent","opaque"]
+    created:str
+    updated:str
+    summary:NotRequired[str]
+    creator:creator_dict
+    organizer:creator_dict
+    start:date_dict
+    end:date_dict
+
+    # solo en cosas q c repiten
+    recurringEventId:NotRequired[str]
+    originalStartTime:NotRequired[date_dict]
+
+    transparency:NotRequired[Literal["transparent","opaque"]]
     iCalUID:str
     sequence:int
-    reminders:bool
+    reminders:reminder_dict
     eventType:Literal["default","birthday","focusTime","fromGmail","outOfOffice","workingLocation"]
+
+hola:agenda = {
+    "kind":"aaa",
+    "etag":"dddddd",
+    "id":"kamlkmlkamskmalk",
+    "status":"tentative",
+    "htmlLink":"lksklmdslk",
+    "created":datetime.now().__str__(),
+    "updated":datetime.now().__str__(),
+    "summary":"eso tilin",
+    "creator": { "email":"correo", "self":False },
+    "organizer": { "email":"safaf", "self":False },
+    "start": { "date":date.today().__str__() },
+    "end": { "date":date.today().__str__() },
+    "transparency":"opaque",
+    "iCalUID":"kasmslkmafmakf",
+    "sequence":0,
+    "reminders": { "useDefault":True },
+    "eventType":"default"
+}
+
+print(hola)
