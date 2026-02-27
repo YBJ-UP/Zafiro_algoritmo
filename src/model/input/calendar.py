e7ff53daf6b2e89a7687d6acb2699d954cd88b2b
Yael <243678@ids.upchiapas.edu.mx>
2026-02-26 22:00:25 -0600

    que pleito, esto de los crudos y procesados lo tengo q ver cn mi equipo pero ya mañana alch

--- src/model/input/calendar.py
+++ src/model/input/calendar.py
@@ -26,7 +26,7 @@ class extras_dict(TypedDict):
     prioridad: Literal["baja","media","alta"]
     etiquetas: list[etiquetas_dict]
 
-class agenda(TypedDict):
+class agenda_raw(TypedDict):
     kind:Literal["calendar#event"]
     etag:str
     id:str
@@ -52,6 +52,26 @@ class agenda(TypedDict):
 
     extras:extras_dict
 
+class agenda_processed(TypedDict):
+    id:str
+    status: Literal["confirmed","tentative","cancelled"]
+    created:str
+    updated:str
+    summary:NotRequired[str]
+    organizer:creator_dict
+    start:date_dict
+    end:date_dict
+
+    # solo en cosas q c repiten
+    recurringEventId:NotRequired[str]
+    originalStartTime:NotRequired[date_dict]
+
+    transparency:NotRequired[Literal["transparent","opaque"]]
+    reminders:reminder_dict
+    eventType:Literal["default","birthday","focusTime","fromGmail","outOfOffice","workingLocation"]
+
+    extras:extras_dict
+
 class actividades_response_raw(TypedDict): # tiene mas atributos pero estos son los q realmente importan
     kind:Literal["calendar#event"]
     etag: str
@@ -62,69 +82,51 @@ class actividades_response_raw(TypedDict): # tiene mas atributos pero estos son 
     defaultReminders:list[reminder_override]
     nextPageToken:NotRequired[str]
     nextSyncToken:NotRequired[str]
-    items: list[agenda]
+    items: list[agenda_raw]
 
 class actividades_response_processed(TypedDict): # tiene mas atributos pero estos son los q realmente importan
     defaultReminders:list[reminder_override]
-    items: list[agenda]
+    items: list[agenda_processed]
 
-hola:agenda = {
-    "kind":"calendar#event",
-    "etag":"dddddd",
+hola:agenda_processed = {
     "id":"kamlkmlkamskmalk",
     "status":"tentative",
-    "htmlLink":"lksklmdslk",
     "created":datetime.now().__str__(),
     "updated":datetime.now().__str__(),
     "summary":"eso tilin",
-    "creator": { "email":"correo", "self":False },
     "organizer": { "email":"safaf", "self":False },
     "start": { "date":date.today().__str__() },
     "end": { "date":date.today().__str__() },
     "transparency":"opaque",
-    "iCalUID":"kasmslkmafmakf",
-    "sequence":0,
     "reminders": { "useDefault":True },
     "eventType":"default",
     "extras": { "etiquetas":[{"etiqueta":"chamba", "color":"#000000"}], "prioridad":"alta" }
 }
-adios:agenda = {
-    "kind":"calendar#event",
-    "etag":"asdsadsad",
+adios:agenda_processed = {
     "id":"fwfwfsscd",
     "status":"tentative",
-    "htmlLink":"lksklmdslk",
     "created":datetime.now().__str__(),
     "updated":datetime.now().__str__(),
     "summary":"noc",
-    "creator": { "email":"correo", "self":False },
     "organizer": { "email":"safaf", "self":False },
     "start": { "date":date.today().__str__() },
     "end": { "date":date.today().__str__() },
     "transparency":"opaque",
-    "iCalUID":"kasmslkmafmakf",
-    "sequence":0,
     "reminders": { "useDefault":True },
     "eventType":"default",
     "extras": { "etiquetas":[{"etiqueta":"chamba", "color":"#000000"}], "prioridad":"alta" }
 }
 
-njkadaskd:agenda = {
-    "kind":"calendar#event",
-    "etag":"wwiikqw",
+njkadaskd:agenda_processed = {
     "id":"jdsdnjas",
     "status":"tentative",
-    "htmlLink":"lksklmdslk",
     "created":datetime.now().__str__(),
     "updated":datetime.now().__str__(),
     "summary":"sepa esto es d prueba",
-    "creator": { "email":"correo", "self":False },
     "organizer": { "email":"safaf", "self":False },
     "start": { "date":date.today().__str__() },
     "end": { "date":date.today().__str__() },
     "transparency":"opaque",
-    "iCalUID":"kasmslkmafmakf",
-    "sequence":0,
     "reminders": { "useDefault":True },
     "eventType":"default",
     "extras": { "etiquetas":[{"etiqueta":"chamba", "color":"#000000"}], "prioridad":"alta" }
