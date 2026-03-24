167e20bd82a7ff355739edb5de8f7d9e00fbd7f1
Yael <243678@ids.upchiapas.edu.mx>
2026-03-24 01:24:06 -0600

    ps el host no se quitaba xd

--- main.py
+++ main.py
@@ -9,4 +9,4 @@ port:int = int(PORT)
 
 def main() -> None:
     if __name__ == "__main__":
-        uvicorn.run(app="src.routes:app", port=port)
+        uvicorn.run(app="src.routes:app", host="0.0.0.0", port=port)
