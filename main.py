62a2a408c0d0cb78d25e39cb539bf6e7b853f440
Yael <243678@ids.upchiapas.edu.mx>
2026-03-24 01:15:14 -0600

    se mueve el puerto al .env y se cambia pq si no no copera el render

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import uvicorn
+from dotenv import load_dotenv
+from os import getenv
+
+load_dotenv()
+PORT: str | None = getenv("PORT")
+assert PORT is not None
+port:int = int(PORT)
 
 def main() -> None:
     if __name__ == "__main__":
-        uvicorn.run(app="src.routes:app", port=8000)
+        uvicorn.run(app="src.routes:app", port=port)
